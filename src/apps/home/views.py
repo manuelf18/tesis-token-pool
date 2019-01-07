c8ce246670cc55e1d7c12d90b54b65d3ab177418
Manuel Fernandez <manuel.fernandez@balboadevelopers.com>
2019-01-07 15:44:50 -0500

    added full pug support

--- src/apps/home/views.py
+++ src/apps/home/views.py
@@ -3,4 +3,4 @@ from django.views.generic import TemplateView
 
 
 class HomeView(TemplateView):
-    template_name = 'index.html'
+    template_name = 'index.pug'
--- src/src/settings/base.py
+++ src/src/settings/base.py
@@ -57,14 +57,25 @@ ROOT_URLCONF = 'src.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+            ],
+            'loaders': [
+                # PyPugJS part:   ##############################
+                ('pypugjs.ext.django.Loader', (
+                    'django.template.loaders.filesystem.Loader',
+                    'django.template.loaders.app_directories.Loader',
+                ))
+            ],
+            'builtins': [
+                'pypugjs.ext.django.templatetags',
             ],
         },
     },
