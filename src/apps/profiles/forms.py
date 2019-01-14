f1e140479799af7646fdcf004d19f36ef2358290
Manuel Fernandez <manuel.fernandez@balboadevelopers.com>
2019-01-14 10:27:55 -0500

    added more to the form

--- src/apps/profiles/forms.py
+++ src/apps/profiles/forms.py
@@ -1,13 +1,22 @@
+from django import forms
+
 from apps.core.forms import BootstrapModelForm
-from django.forms import PasswordInput
 
 from .models import User
 
 
 class UserModelForm(BootstrapModelForm):
+    confirm_password = forms.CharField(label='Confirmar Contraseña', widget=forms.PasswordInput(), required=True)
+
     class Meta:
         model = User
-        fields = ('name', 'email', 'password', 'identification', 'location')
+        fields = ('name', 'email', 'password', 'confirm_password', 'identification', 'location')
         widgets = {
-            'password': PasswordInput
+            'password': forms.PasswordInput()
         }
+
+    def save(self, commit=True):
+        # do something
+        if self.cleaned_data['password'] is not self.cleaned_data['confirm_password']:
+            raise ValueError('Passwords do not match')
+        super().save(commit=commit)
